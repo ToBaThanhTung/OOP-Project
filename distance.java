dd3ac67bd86c1882d72ab917addb62aeceafe89f
tuanngociuit98 <33689489+tuanngociuit98@users.noreply.github.com>
2017-11-29 21:41:32 +0700

    Add files via upload

---
+++ distance.java
@@ -0,0 +1,24 @@
+package obj;
+
+public class distance {
+ int begin;
+ int end;
+ public distance(int begin,int end)
+ {
+	 this.begin=begin;
+	 this.end= end;
+ }
+public int getBegin() {
+	return begin;
+}
+public void setBegin(int begin) {
+	this.begin = begin;
+}
+public int getEnd() {
+	return end;
+}
+public void setEnd(int end) {
+	this.end = end;
+}
+ 
+}
